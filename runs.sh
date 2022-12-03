608807224fd6213885eb6ec1d95c8dc703d069a8
ribhavb <boseribhav@gmail.com>
2022-12-03 14:27:16 -0600

    adding script

---
+++ runs.sh
@@ -0,0 +1,66 @@
+module load nvidia 
+module load conda 
+
+conda activate 
+pip install -r requirements.txt
+
+
+python -u -m torch.distributed.launch --nproc_per_node 4 transformers/examples/pytorch/text-classification/run_glue.py \
+  --model_name_or_path ~/deep_learning/BERT-Tickets-DL/full_prune/bert-0.3 \
+  --task_name mnli \
+  --tokenizer_name "bert-base-uncased" \
+  --do_train \
+  --do_eval \
+  --max_seq_length 128 \
+  --per_device_train_batch_size 32 \
+  --learning_rate 2e-5 \
+  --num_train_epochs 1.0 \
+  --output_dir full-results/bert-0.3/mnli
+
+python -u -m torch.distributed.launch --nproc_per_node 4 transformers/examples/pytorch/text-classification/run_glue.py \
+  --model_name_or_path ~/deep_learning/BERT-Tickets-DL/full_prune/bert-0.3 \
+  --task_name sst2 \
+  --tokenizer_name "bert-base-uncased" \
+  --do_train \
+  --do_eval \
+  --max_seq_length 128 \
+  --per_device_train_batch_size 32 \
+  --learning_rate 2e-5 \
+  --num_train_epochs 2.0 \
+  --output_dir full-results/bert-0.3/sst2
+
+python -u -m torch.distributed.launch --nproc_per_node 4 transformers/examples/pytorch/text-classification/run_glue.py \
+  --model_name_or_path ~/deep_learning/BERT-Tickets-DL/full_prune/bert-0.3 \
+  --task_name qnli \
+  --tokenizer_name "bert-base-uncased" \
+  --do_train \
+  --do_eval \
+  --max_seq_length 128 \
+  --per_device_train_batch_size 32 \
+  --learning_rate 2e-5 \
+  --num_train_epochs 1.0 \
+  --output_dir full-results/bert-0.3/qnli
+
+python -u -m torch.distributed.launch --nproc_per_node 4 transformers/examples/pytorch/text-classification/run_glue.py \
+  --model_name_or_path ~/deep_learning/BERT-Tickets-DL/full_prune/bert-0.3 \
+  --task_name stsb \
+  --tokenizer_name "bert-base-uncased" \
+  --do_train \
+  --do_eval \
+  --max_seq_length 128 \
+  --per_device_train_batch_size 32 \
+  --learning_rate 2e-5 \
+  --num_train_epochs 5.0 \
+  --output_dir full-results/bert-0.3/stsb
+
+python -u -m torch.distributed.launch --nproc_per_node 4 transformers/examples/pytorch/text-classification/run_glue.py \
+  --model_name_or_path ~/deep_learning/BERT-Tickets-DL/full_prune/bert-0.3 \
+  --task_name mrpc \
+  --tokenizer_name "bert-base-uncased" \
+  --do_train \
+  --do_eval \
+  --max_seq_length 128 \
+  --per_device_train_batch_size 32 \
+  --learning_rate 2e-5 \
+  --num_train_epochs 5.0 \
+  --output_dir full-results/bert-0.3/mrpc
