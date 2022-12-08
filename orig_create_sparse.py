82eb0825c4054548a57d32e1fd724d38efe539b7
ribhavb <boseribhav@gmail.com>
2022-12-08 01:31:39 -0600

    mixed runs

--- orig_create_sparse.py
+++ orig_create_sparse.py
@@ -112,6 +112,54 @@ def pruning_model_lower(model,px):
     
     return model
 
+
+def pruning_model_upper_lower(model,px_u, px_l):
+
+    parameters_to_prune =[]
+
+    for ii in range(6):
+        parameters_to_prune.append((model.encoder.layer[ii].attention.self.query, 'weight'))
+        parameters_to_prune.append((model.encoder.layer[ii].attention.self.key, 'weight'))
+        parameters_to_prune.append((model.encoder.layer[ii].attention.self.value, 'weight'))
+        parameters_to_prune.append((model.encoder.layer[ii].attention.output.dense, 'weight'))
+        parameters_to_prune.append((model.encoder.layer[ii].intermediate.dense, 'weight'))
+        parameters_to_prune.append((model.encoder.layer[ii].output.dense, 'weight'))
+
+    parameters_to_prune.append((model.pooler.dense, 'weight'))
+    parameters_to_prune = tuple(parameters_to_prune)
+
+    prune.global_unstructured(
+        parameters_to_prune,
+        pruning_method=prune.L1Unstructured,
+        amount=px_u*2,
+    )
+
+    parameters_to_prune = [] 
+    for i in range(6):
+        ii = i + 6
+        parameters_to_prune.append((model.encoder.layer[ii].attention.self.query, 'weight'))
+        parameters_to_prune.append((model.encoder.layer[ii].attention.self.key, 'weight'))
+        parameters_to_prune.append((model.encoder.layer[ii].attention.self.value, 'weight'))
+        parameters_to_prune.append((model.encoder.layer[ii].attention.output.dense, 'weight'))
+        parameters_to_prune.append((model.encoder.layer[ii].intermediate.dense, 'weight'))
+        parameters_to_prune.append((model.encoder.layer[ii].output.dense, 'weight'))
+
+    parameters_to_prune.append((model.pooler.dense, 'weight'))
+    parameters_to_prune = tuple(parameters_to_prune)
+
+    prune.global_unstructured(
+        parameters_to_prune,
+        pruning_method=prune.L1Unstructured,
+        amount=px_l*2,
+    )
+
+    for (layer,wt) in parameters_to_prune:
+        print('in remove')
+        print(list(layer.named_buffers()))
+        prune.remove(layer, wt)
+    
+    return model
+
 def see_weight_rate(model):
 
     sum_list = 0
@@ -135,12 +183,15 @@ def see_weight_rate(model):
         sum_list = sum_list+float(model.encoder.layer[ii].output.dense.weight.nelement())
         zero_sum = zero_sum+float(torch.sum(model.encoder.layer[ii].output.dense.weight == 0))
 
+        print('ii: ', zero_sum/sum_list)
+        zero_sum = 0
+        sum_list = 0
 
-    sum_list = sum_list+float(model.pooler.dense.weight.nelement())
-    zero_sum = zero_sum+float(torch.sum(model.pooler.dense.weight == 0))
+
+    # sum_list = sum_list+float(model.pooler.dense.weight.nelement())
+    # zero_sum = zero_sum+float(torch.sum(model.pooler.dense.weight == 0))
  
 
-    return 100*zero_sum/sum_list
 
 
 config = BertConfig.from_pretrained(
@@ -152,20 +203,21 @@ config = BertConfig.from_pretrained(
 #             config=config
 #         )
 # model.save_pretrained(f"full_prune/bert-base")
+sparsity_values_u = [0.3,0.33,0.43,0.415]
+sparsity_values_l = [0.4,0.37,0.47,0.485] 
 
-sparsity_values = [0.7,0.9,0.95,0.99]
-# sparsity_values = [0.495]
 
-
-for s in sparsity_values:
+for i in range(len(sparsity_values_u)):
     model = BertModel.from_pretrained(
             'bert-base-uncased',
             from_tf=bool(".ckpt" in 'bert-base-uncased'),
             config=config
         )
-    model = pruning_model_random(model,s)
+    upper = sparsity_values_u[i]
+    lower = sparsity_values_l[i]
+    model = pruning_model_upper_lower(model,upper,lower)
 
     zero = see_weight_rate(model)
     print('zero rate', zero)
 
-    model.save_pretrained(f"upper_prune/bert-{s}")
+    model.save_pretrained(f"full_prune/bert-upper-{upper}-lower-{lower}")
